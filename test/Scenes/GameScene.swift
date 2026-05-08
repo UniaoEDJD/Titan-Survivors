6dcef68d6ba8ebc62e806220bf74c1c0bdf96e5f
Gonki <goncaloaraujo2004@gmail.com>
2026-05-08 10:15:24 +0100

    joystick and temporary player

--- test/Scenes/GameScene.swift
+++ test/Scenes/GameScene.swift
@@ -9,80 +9,57 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var player: SKSpriteNode!
+    var cameraNode: SKCameraNode!
+    var joystick: VirtualJoystick!
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let playerSpeed: CGFloat = 5.0
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        setupBackground()
+        setupPlayer()
+        setupCamera()
+        setupJoystick()
+    }
+    
+    func setupBackground() {
+        let background = SKSpriteNode(color: SKColor.darkGray, size: CGSize(width: 3000, height: 3000))
+        background.zPosition = -1
+        addChild(background)
+    }
+    
+    func setupPlayer() {
+            // Criar o jogador (um quadrado azul por agora)
+            player = SKSpriteNode(color: .systemBlue, size: CGSize(width: 40, height: 40))
+            player.position = .zero
+            addChild(player)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        func setupCamera() {
+            cameraNode = SKCameraNode()
+            self.camera = cameraNode // Diz à Scene que esta é a câmara principal
+            addChild(cameraNode)
+        }
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        func setupJoystick() {
+            joystick = VirtualJoystick()
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            // Posicionar no canto inferior esquerdo do ecrã
+            let xPos = -(size.width / 2) + 100
+            let yPos = -(size.height / 2) + 100
+            joystick.position = CGPoint(x: xPos, y: yPos)
+            
+            // ATENÇÃO: Adicionar à câmara para ficar sempre visível!
+            cameraNode.addChild(joystick)
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+        override func update(_ currentTime: TimeInterval) {
+            // 1. Atualizar a posição do jogador com base no joystick
+            if joystick.velocity != .zero {
+                player.position.x += joystick.velocity.x * playerSpeed
+                player.position.y += joystick.velocity.y * playerSpeed
+            }
+            
+            // 2. A câmara persegue a posição do jogador
+            cameraNode.position = player.position
         }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
---
+++ test/UI/VirtualJoystick.swift
@@ -0,0 +1,75 @@
+//
+//  VirtualJoystick.swift
+//  test
+//
+//  Created by Gonçalo Araújo on 08/05/2026.
+//
+
+import SpriteKit
+
+class VirtualJoystick: SKNode {
+    private let baseNode: SKShapeNode
+    private let knobNode: SKShapeNode
+    private let maxRadius: CGFloat = 50.0
+    
+    var velocity: CGPoint = .zero
+    
+    override init() {
+        baseNode = SKShapeNode(circleOfRadius: maxRadius)
+        baseNode.fillColor = SKColor.gray.withAlphaComponent(0.5)
+        baseNode.strokeColor = .clear
+        
+        knobNode = SKShapeNode(circleOfRadius: 25.0)
+        knobNode.fillColor = SKColor.white
+        knobNode.strokeColor = .clear
+        
+        super.init()
+        
+        addChild(baseNode)
+        addChild(knobNode)
+        
+        isUserInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            moveKnob(to: touches.first?.location(in: self) ?? .zero)
+        }
+        
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            moveKnob(to: touches.first?.location(in: self) ?? .zero)
+        }
+        
+        override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            resetKnob()
+        }
+        
+        override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+            resetKnob()
+        }
+    
+    private func moveKnob(to location: CGPoint) {
+            let dx = location.x
+            let dy = location.y
+            let distance = sqrt(dx * dx + dy * dy)
+            
+            if distance <= maxRadius {
+                knobNode.position = location
+                velocity = CGPoint(x: dx / maxRadius, y: dy / maxRadius)
+            } else {
+                let angle = atan2(dy, dx)
+                knobNode.position = CGPoint(x: cos(angle) * maxRadius, y: sin(angle) * maxRadius)
+                velocity = CGPoint(x: cos(angle), y: sin(angle))
+            }
+        }
+        
+        private func resetKnob() {
+            let moveBack = SKAction.move(to: .zero, duration: 0.1)
+            moveBack.timingMode = .easeOut
+            knobNode.run(moveBack)
+            velocity = .zero
+        }
+}
