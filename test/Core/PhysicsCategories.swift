868019d3260540e0b77174853518e61467218bc7
Gonki <goncaloaraujo2004@gmail.com>
2026-05-22 10:57:04 +0100

    stuff

--- test/Core/PhysicsCategories.swift
+++ test/Core/PhysicsCategories.swift
@@ -4,4 +4,5 @@ struct PhysicsCategories{
     static let none: UInt32 = 0
     static let player: UInt32 = 0b1
     static let enemy: UInt32 = 0b10
+    static let expGem: UInt32 = 0b100
 }
--- test/Entities/Items/ExperienceNode.swift
+++ test/Entities/Items/ExperienceNode.swift
@@ -1,7 +1,24 @@
-//
-//  ExperienceNode.swift
-//  test
-//
-//  Created by Gonçalo Araújo on 08/05/2026.
-//
+import SpriteKit
 
+class ExperienceNode: SKSpriteNode {
+    let xpValue: Int
+    
+    init(value: Int = 1) {
+        self.xpValue = value
+        
+        super.init(texture: nil, color: .yellow, size: CGSize(width: 12, height: 12))
+        self.name = "experience"
+        
+        self.physicsBody = SKPhysicsBody(circleOfRadius: 6)
+        self.physicsBody?.isDynamic = false
+        self.physicsBody?.affectedByGravity = false
+        
+        self.physicsBody?.categoryBitMask = PhysicsCategories.expGem
+        self.physicsBody?.collisionBitMask = 0
+        self.physicsBody?.contactTestBitMask = PhysicsCategories.player
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
